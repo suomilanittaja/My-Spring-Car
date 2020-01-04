c1a7faf9c83049db42a3761401dd02890c3dba3a
Suomilanittaja <unitypeli@gmail.com>
2020-01-04 22:22:22 +0200

    added door opening script

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+   public class OpenDoor : MonoBehaviour
+    {
+        public bool open = false;
+        Animator anim;
+     
+        void Start ()
+        {
+            anim = GetComponentInChildren<Animator> ();
+            Debug.Log("GOT THE ANIMATOR");
+        }
+     
+        void OnTriggerEnter (Collider other)
+     
+            {
+            if (other.CompareTag("Player"))
+            {
+                anim.SetBool ("open", true);
+                Debug.Log ("OPENING THE DOOR");
+            }
+     
+        }
+     
+        void OnTriggerExit (Collider other)
+     
+            {
+            if (other.CompareTag("Player"))
+            {
+                anim.SetBool ("open", false);
+                Debug.Log ("OPENING THE DOOR");
+            }
+        }
+    }
--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -6,6 +6,7 @@ public class PickUp : MonoBehaviour
 {
 	public Transform theDest;
 	
+
 	void OnMouseDown()
 	{
 		
