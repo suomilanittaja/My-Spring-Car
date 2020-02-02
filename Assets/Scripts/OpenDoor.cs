e39e5e7239ba8c27a4216afe6d4f9df002a3ffdf
suomilanittaja <randomvlog63@gmail.com>
2020-02-02 15:15:38 +0200

    Upgraded text to textMesh pro

--- Assets/Scripts/OpenDoor.cs
+++ Assets/Scripts/OpenDoor.cs
@@ -6,6 +6,9 @@ using System.Collections;
         public bool open = false;
         Animator anim;
         private bool Pressed;
+        public GameObject textOpen;
+        public GameObject textClose;
+
         void Start ()
         {
             anim = GetComponentInChildren<Animator> ();
@@ -40,4 +43,23 @@ using System.Collections;
      
         }
 
+        void OnTriggerEnter (Collider other)
+        {
+            if (other.CompareTag("Player") && open == false)
+            {
+                textOpen.gameObject.SetActive(true);
+                textClose.gameObject.SetActive(false);
+            }
+            else if (other.CompareTag("Player") && open == true)
+            {
+                textOpen.gameObject.SetActive(false);
+                textClose.gameObject.SetActive(true);
+            }
+        }
+
+        void OnTriggerExit (Collider other)
+        {
+            textOpen.gameObject.SetActive(false);
+            textClose.gameObject.SetActive(false);
+        }
     }
