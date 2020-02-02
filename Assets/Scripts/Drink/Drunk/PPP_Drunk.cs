491aa8b8eb9549c4069164081176212e1505fcb1
suomilanittaja <randomvlog63@gmail.com>
2020-02-02 16:51:34 +0200

    Rewrited drunk script (Beta) Which fix multiple Bug and fixes and use

---
+++ Assets/Scripts/Drink/Drunk/PPP_Drunk.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+ 
+[Serializable]
+[PostProcess(typeof(PPP_DrunkRenderer), PostProcessEvent.AfterStack, "Custom/PPP_Drunk")]
+public sealed class PPP_Drunk : PostProcessEffectSettings
+{
+    [Range(0f, 1f), Tooltip("Drunk Distortion effect intensity.")]
+    public FloatParameter amplitude = new FloatParameter { value = 0.5f };
+
+    [Range(0f, 100f), Tooltip("Drunk Distortion effect frequency.")]
+    public FloatParameter frequency = new FloatParameter { value = 0.5f };
+
+    [Tooltip("Speed of distorion only x&y used")]
+    public Vector4Parameter speed = new Vector4Parameter { value = new Vector4(1, 1, 0, 0)};
+
+    [Tooltip("Size of distorion only x&y used")]
+    public Vector4Parameter size = new Vector4Parameter { value = new Vector4(1, 1, 0, 0)};
+}
+ 
+public sealed class PPP_DrunkRenderer : PostProcessEffectRenderer<PPP_Drunk>
+{
+    public override void Render(PostProcessRenderContext context)
+    {
+        var sheet = context.propertySheets.Get(Shader.Find("Custom/PPP_Drunk"));
+        sheet.properties.SetFloat("_Amplitude", settings.amplitude);
+        sheet.properties.SetFloat("_Frequency", settings.frequency);
+        sheet.properties.SetVector("_Speed", settings.speed);
+        sheet.properties.SetVector("_Size", settings.size);
+        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+    }
+}
---
+++ Assets/Shaders/PPP_Drunk.shader
@@ -0,0 +1,42 @@
+﻿Shader "Custom/PPP_Drunk"
+{
+    HLSLINCLUDE
+
+        #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+
+        TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+        float _Amplitude;
+        float _Frequency;
+        float2 distortion;
+
+        float4 _Speed;
+        float4 _Size;
+
+        float4 Frag(VaryingsDefault i) : SV_Target
+        {
+            float2 uv = i.texcoord;
+            distortion.x = (sin((_Speed.x * _Time.y) + (uv.x * _Size.x) * _Frequency)) * _Amplitude;
+            distortion.y = (cos((_Speed.y * _Time.y) + (uv.y * _Size.y) * _Frequency)) * _Amplitude;
+            float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv + distortion);
+            //float luminance = dot(color.rgb, float3(0.2126729, 0.7151522, 0.0721750));
+            //color.rgb = lerp(color.rgb, luminance.xxx, _Blend.xxx);
+            return color;
+        }
+
+    ENDHLSL
+
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            HLSLPROGRAM
+
+                #pragma vertex VertDefault
+                #pragma fragment Frag
+
+            ENDHLSL
+        }
+    }
+}
